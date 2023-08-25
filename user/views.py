f96f1b1d817ae752702adee817f921353edc06e2
Furkan <furkanilaslan@gmail.com>
2023-08-25 16:00:07 +0300

    second commit

--- user/views.py
+++ user/views.py
@@ -27,6 +27,7 @@ def userLogin(request):
 
         if kullanici is not None:
             login(request,user)
+            print("Merhaba")
             return redirect('index')
 
     return render(request, 'login.html')
